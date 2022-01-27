5e6e045164e035387dacda4bd3bca3394e5cc1e3
dedyrosyadi <dedy.rosyadi@gmail.com>
2022-01-27 19:43:58 +0800

    merapikan footer,nav dan sidebar

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-  <div class="float-right d-none d-sm-inline">
-    Anything you want
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 0.1.0
   </div>
-  <strong>Copyright &copy; 2022 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  <strong>Copyright &copy; 2022 <a href="https://adminlte.io">Praktikum Web</a>.</strong> All rights reserved.
 </footer>
 </div>
