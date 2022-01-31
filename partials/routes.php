3c808d729138c958183b4aa0d4b9ad44b11e5b44
dedyrosyadi <dedy.rosyadi@gmail.com>
2022-01-31 12:50:32 +0800

    menampilkan data halaman lokasi

--- partials/routes.php
+++ partials/routes.php
@@ -1,17 +1,20 @@
-<?php 
+<?php
 if (isset($_GET['page'])) {
   $page = $_GET['page'];
 
   switch ($page) {
     case '';
-    case 'home' :
+    case 'home':
       file_exists('pages/home.php') ? include 'page/home/php' : include "pages/404.php";
+      break;
+    case 'lokasiread':
+      file_exists('pages/admin/lokasiread.php') ? include
+        'pages/admin/lokasiread.php' : include "pages/404.php";
       break;
 
     default:
       include "pages/404.php";
   }
-
 } else {
   include "pages/home.php";
 }
--- partials/scriptsdatatables.php
+++ partials/scriptsdatatables.php
@@ -5,5 +5,5 @@
 <script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
 <script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
 <script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="plugins/datatables-buttons/js/buttons.print.min.js/"></script>
+<script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
 <script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
